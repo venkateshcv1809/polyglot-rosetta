16275fd828096bd10cfe8b144b3bffbc1290d65f
VENKATESH C V <venkateshvairam@gmail.com>
2026-08-24 22:35:48 +0530

    feat: Add constants for default values

---
+++ pkg/constants/default.go
@@ -0,0 +1,19 @@
+package constants
+
+// Directory Paths
+const (
+	SrcDir      = "./src"
+	TemplateDir = "./.template"
+)
+
+// Root Category Metadata
+const (
+	RootID          = "root"
+	RootTitle       = "Polyglot Rosetta"
+	RootDescription = "Catalog"
+)
+
+// Concept Option Fallbacks
+const (
+	ConceptDifficulty = "Easy"
+)
